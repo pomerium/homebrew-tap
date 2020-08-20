0ecee5a20d42bcf70f2787ea2f48574411297a57
apparitor <apparitor@users.noreply.github.com>
2020-08-20 13:37:34 -0400

    Brew formula update for pomerium version v0.10.1

--- Formula/pomerium-cli.rb
+++ Formula/pomerium-cli.rb
@@ -2,24 +2,24 @@
 class PomeriumCli < Formula
   desc ""
   homepage ""
-  version "0.10.0"
+  version "0.10.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/pomerium/pomerium/releases/download/v0.10.0/pomerium-cli-darwin-amd64.tar.gz"
-    sha256 "f982f0009b6b9934b219d5251dc21861af40e1d57d7cc977eabb344f9c246aaf"
+    url "https://github.com/pomerium/pomerium/releases/download/v0.10.1/pomerium-cli-darwin-amd64.tar.gz"
+    sha256 "11270c34c9349948e4a3e4788942fa9d0d457c8482bb8120958190301c1d4131"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/pomerium/pomerium/releases/download/v0.10.0/pomerium-cli-linux-amd64.tar.gz"
-      sha256 "3282b5a59030534890a3c829b5048c0aadbbea7e0c727bd8bde7fae0f7eaaea6"
+      url "https://github.com/pomerium/pomerium/releases/download/v0.10.1/pomerium-cli-linux-amd64.tar.gz"
+      sha256 "b56e86ef66ec47e2952d2b3db5f28633e4b75cd165a82a9982fa75080f202629"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/pomerium/pomerium/releases/download/v0.10.0/pomerium-cli-linux-arm64.tar.gz"
-        sha256 "8ad64b63c8776103a144d4cde86939e25047bc4df26341a57d02f1ba3c8aaee0"
+        url "https://github.com/pomerium/pomerium/releases/download/v0.10.1/pomerium-cli-linux-arm64.tar.gz"
+        sha256 "ee500d68f884ec23ded4f530d9d608ee4a6b7ba6f73cf58f082a13c0207ce85b"
       else
-        url "https://github.com/pomerium/pomerium/releases/download/v0.10.0/pomerium-cli-linux-armv6.tar.gz"
-        sha256 "7541e521dfee3518eb1b3eea540af47dc33d64a900663e5cb4e07c24194fa602"
+        url "https://github.com/pomerium/pomerium/releases/download/v0.10.1/pomerium-cli-linux-armv6.tar.gz"
+        sha256 "185f3dc8ab33ca3d4d93657832fdac6a91eb7941f84e46d5a82b510292ac5569"
       end
     end
   end
