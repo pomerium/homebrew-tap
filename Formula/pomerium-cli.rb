c8220cf1b5d7b25992c1dff3c72a3cd7fd2e1950
apparitor <apparitor@users.noreply.github.com>
2021-02-23 07:32:44 -0800

    Brew formula update for pomerium version v0.13.1

--- Formula/pomerium-cli.rb
+++ Formula/pomerium-cli.rb
@@ -5,24 +5,24 @@
 class PomeriumCli < Formula
   desc ""
   homepage ""
-  version "0.13.0"
+  version "0.13.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/pomerium/pomerium/releases/download/v0.13.0/pomerium-cli-darwin-amd64.tar.gz"
-    sha256 "97d5fc504d3b78c9f108d436c73f2dff34167a2e6751869670a89a3afd47dd24"
+    url "https://github.com/pomerium/pomerium/releases/download/v0.13.1/pomerium-cli-darwin-amd64.tar.gz"
+    sha256 "6ef976b259aca34fa6f68e84bd6947df9663ebf11073e4a3056381c931858559"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/pomerium/pomerium/releases/download/v0.13.0/pomerium-cli-linux-amd64.tar.gz"
-    sha256 "18df726a658c91d5ddd52abfcae109916062f769585e8cf82ca95c335b653403"
+    url "https://github.com/pomerium/pomerium/releases/download/v0.13.1/pomerium-cli-linux-amd64.tar.gz"
+    sha256 "31e26a612faeec741f465fdb168c4e3262709ad0bae469651ea8430b907acf99"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/pomerium/pomerium/releases/download/v0.13.0/pomerium-cli-linux-armv6.tar.gz"
-    sha256 "003a83eaa634bebe871ada4a9a03677ef6c161ce05a64aa50fc396c05d307341"
+    url "https://github.com/pomerium/pomerium/releases/download/v0.13.1/pomerium-cli-linux-armv6.tar.gz"
+    sha256 "6e826a2e4bd8cd90325cd3852c55e9c70c67d1ff5826bbd67117cf374df1fd3a"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/pomerium/pomerium/releases/download/v0.13.0/pomerium-cli-linux-arm64.tar.gz"
-    sha256 "b2037ef2f670bf44b02d1425f13cbe8540f9552d21606e969cfcf826855a80cd"
+    url "https://github.com/pomerium/pomerium/releases/download/v0.13.1/pomerium-cli-linux-arm64.tar.gz"
+    sha256 "cb2f2cbf4b47d05ca6610c1dfbc9a9f3918c90f5b7f68cc44133f6cf89da2934"
   end
 
   def install
