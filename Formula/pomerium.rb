bdcc1c089eea70b5989279a648d5a668a9077cc0
apparitor <apparitor@users.noreply.github.com>
2021-05-14 09:01:12 -0400

    Brew formula update for pomerium version v0.14.1

--- Formula/pomerium.rb
+++ Formula/pomerium.rb
@@ -5,20 +5,20 @@
 class Pomerium < Formula
   desc ""
   homepage ""
-  version "0.14.0"
+  version "0.14.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/pomerium/pomerium/releases/download/v0.14.0/pomerium-darwin-amd64.tar.gz"
-    sha256 "b2e96121e57b1cb96c2d8f902d6c474e3b491dac84c4ad5b3fa62e113bada212"
+    url "https://github.com/pomerium/pomerium/releases/download/v0.14.1/pomerium-darwin-amd64.tar.gz"
+    sha256 "a259d3dec925956aada3fc3ef6adec62a4e7686b2336af8cbc0533efd663c0f8"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/pomerium/pomerium/releases/download/v0.14.0/pomerium-linux-amd64.tar.gz"
-    sha256 "bcc6c24086e876d977d7ef073a59d0fbd317a119bcece58cfba45289dde319f8"
+    url "https://github.com/pomerium/pomerium/releases/download/v0.14.1/pomerium-linux-amd64.tar.gz"
+    sha256 "16068fd0f0e8073c160ccfc49db8f2eb057304bf0d4f47902946297719f48325"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/pomerium/pomerium/releases/download/v0.14.0/pomerium-linux-arm64.tar.gz"
-    sha256 "b131edfd373a354a8d982f14a147fb22c1601cf6dbc31398e8b28d9870f71010"
+    url "https://github.com/pomerium/pomerium/releases/download/v0.14.1/pomerium-linux-arm64.tar.gz"
+    sha256 "dc05b608e58b7058b27ad18e961e01e29e95a4ea088f0b928775e04f487fce76"
   end
 
   def install
