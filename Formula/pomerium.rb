177b7c657e6d0b799eb81d5b06bcd1252d842c23
apparitor <apparitor@users.noreply.github.com>
2022-01-26 18:09:23 -0500

    Brew formula update for pomerium version v0.16.2

--- Formula/pomerium.rb
+++ Formula/pomerium.rb
@@ -5,12 +5,12 @@
 class Pomerium < Formula
   desc ""
   homepage ""
-  version "0.16.1"
+  version "0.16.2"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/pomerium/pomerium/releases/download/v0.16.1/pomerium-darwin-amd64.tar.gz"
-      sha256 "9675bd5b453aa3d49b6c02dd26b5e757dd235f499574e6a680c6ca29681078b1"
+      url "https://github.com/pomerium/pomerium/releases/download/v0.16.2/pomerium-darwin-amd64.tar.gz"
+      sha256 "a44c6bb5bf1ffb37524a84695ad1d868bce1a7616d348dc94b0a97cf4546eab4"
 
       def install
         bin.install "pomerium"
@@ -20,16 +20,16 @@ class Pomerium < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/pomerium/pomerium/releases/download/v0.16.1/pomerium-linux-arm64.tar.gz"
-      sha256 "e9a89abbce93a58406d544618f065e6f068479690706a8467c180e66b10c0ce4"
+      url "https://github.com/pomerium/pomerium/releases/download/v0.16.2/pomerium-linux-arm64.tar.gz"
+      sha256 "625e6de1e033b35163de3ca7e468f21420f89fadc8d17fc99fda5e449b2d4c7d"
 
       def install
         bin.install "pomerium"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/pomerium/pomerium/releases/download/v0.16.1/pomerium-linux-amd64.tar.gz"
-      sha256 "71a1865858cc7b816191e51bd1a926f1fabe38aaea0250b6a8c733e27f63afc3"
+      url "https://github.com/pomerium/pomerium/releases/download/v0.16.2/pomerium-linux-amd64.tar.gz"
+      sha256 "2ad8d08b759b7d6e99730914e6f61ae5c4f8ab79ef688742fdeaa42567ae5ac5"
 
       def install
         bin.install "pomerium"
