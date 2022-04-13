2579c6d85b987050ef98f8766e9baf6ede0c77ff
apparitor <apparitor@users.noreply.github.com>
2022-04-13 21:13:21 +0000

    Apply automatic changes

--- Casks/pomerium-desktop.rb
+++ Casks/pomerium-desktop.rb
@@ -2,15 +2,15 @@ cask "pomerium-desktop" do
   name "Pomerium Desktop"
   homepage "https://www.pomerium.com"
   desc ""
-  version "homebrew"
+  version "v0.17.1"
 
   app "Pomerium Desktop.app"
 
   if Hardware::CPU.intel?
     sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
-    url "https://github.com/pomerium/desktop-client/releases/download/vhomebrew/Pomerium-Desktop-homebrew.dmg"
+    url "https://github.com/pomerium/desktop-client/releases/download/vv0.17.1/Pomerium-Desktop-v0.17.1.dmg"
   else
     sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
-    url "https://github.com/pomerium/desktop-client/releases/download/vhomebrew/Pomerium-Desktop-homebrew-arm64.dmg"
+    url "https://github.com/pomerium/desktop-client/releases/download/vv0.17.1/Pomerium-Desktop-v0.17.1-arm64.dmg"
   end
 end
