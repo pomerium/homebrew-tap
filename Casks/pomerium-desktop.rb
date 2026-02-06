e5a93c3628eb0eb5cf6e4e0914dc32fb34b09331
apparitor <59841262+apparitor@users.noreply.github.com>
2026-02-06 17:45:56 +0000

    update pomerium-desktop cask for version v0.32.0

--- Casks/pomerium-desktop.rb
+++ Casks/pomerium-desktop.rb
@@ -2,15 +2,15 @@ cask "pomerium-desktop" do
   name "Pomerium Desktop"
   homepage "https://www.pomerium.com"
   desc ""
-  version "0.32.1"
+  version "0.32.0"
 
   app "Pomerium Desktop.app"
 
   if Hardware::CPU.intel?
     sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
-    url "https://github.com/pomerium/desktop-client/releases/download/v0.32.1/Pomerium-Desktop-0.32.1.dmg"
+    url "https://github.com/pomerium/desktop-client/releases/download/v0.32.0/Pomerium-Desktop-0.32.0.dmg"
   else
     sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
-    url "https://github.com/pomerium/desktop-client/releases/download/v0.32.1/Pomerium-Desktop-0.32.1-arm64.dmg"
+    url "https://github.com/pomerium/desktop-client/releases/download/v0.32.0/Pomerium-Desktop-0.32.0-arm64.dmg"
   end
 end
